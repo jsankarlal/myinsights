7bc9829afde38c43897aa2d381112b9ae44eaa95
sankarlal <jsankarlal@gmail.com>
2018-01-12 18:21:54 +0530

    add KPI back

---
+++ AZ_Suggestions_Rep_Territory/js/kpi.js
@@ -0,0 +1,67 @@
+(function(global) {
+    function Hcp() {
+     // Constructor
+    };
+    
+    Hcp.prototype.bindHcpEvents = function() {
+        var _this = this,
+            $document = $(document);
+        $document.on('click', '[data-account-id]', function(e) {
+            var $this = $(this);
+               
+        });
+    }
+    
+    Hcp.prototype.renderHcp = function() {
+        var _this = this;
+        //fillTemplate(container, templateObj, object, appendFlag, callback)
+        _this.fillTemplate(_this.hcp.detailsContainer, componentsTemplate[_this.hcpDetailTemplatePath], resource[_this.hcpDataPath], false);
+        _this.fillTemplate(_this.hcp.listContainer, componentsTemplate[_this.hcpListTemplatePath], resource[_this.hcpDataPath], false);
+    }
+    
+    Hcp.prototype.buildHcp = function() {
+        var _this = this;
+            _this.hcpListTemplatePath = 'hcp-list';
+            _this.hcpDetailTemplatePath = 'hcp-detail';
+            _this.hcpDataPath = '/staticJson/hcp.json';
+       /* _this.fetchResource(_this.hcpDataPath, 'json').then(function() {
+            _this.renderHcp();
+        });
+        */
+        
+        $.ajax({
+            method: 'GET',
+            url: _this.hcpDataPath,
+            type: 'json',
+            success: function(data) {
+                var path = this.url;
+                resource[this.url] = data;
+                _this.renderHcp();
+            },
+
+            error: function(err) {
+            }
+        });
+    }
+    
+    Hcp.prototype.init = function() {
+        var _this = this;
+        _this.hcp = {};
+        _this.hcp.listContainer = $('#hcp-list');
+        _this.hcp.detailsContainer = $('#hcp-details');
+        _this.addSpinner(_this.hcp.listContainer);
+        _this.addSpinner(_this.hcp.detailsContainer);
+        if (_this.application != 'iRep') {
+            _this.buildHcp();
+        }
+            
+    }
+    
+    _.extend(Hcp.prototype, Util.prototype);
+    global.Hcp = Hcp;
+}(this));
+
+$(function() {
+    var hcp = new Hcp();
+    hcp.init();
+});
