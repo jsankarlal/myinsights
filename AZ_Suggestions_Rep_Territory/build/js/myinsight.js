cf3795bd53abbdc91604aeff7108cf088a005945
sankarlal <jsankarlal@gmail.com>
2018-01-23 21:29:17 +0530

    suggestion as dynamic from iRep

--- AZ_Suggestions_Rep_Territory/build/js/myinsight.js
+++ AZ_Suggestions_Rep_Territory/build/js/myinsight.js
@@ -31279,36 +31279,21 @@ $(function() {
      Suggestions.prototype.renderSuggestions = function() {
         var _this = this;
         //fillTemplate(container, templateObj, object, appendFlag, callback)
-    //    _this.fillTemplate(_this.suggestion.listContainer, componentsTemplate[_this.suggestionListTemplatePath], resource[_this.suggestionDataPath], false);
-    //    _this.fillTemplate(_this.suggestion.detailsContainer, componentsTemplate[_this.suggestionDetailTemplatePath], resource[_this.suggestionDataPath], false);
         _this.fillTemplate(_this.suggestion.listContainer, componentsTemplate[_this.suggestionListTemplatePath], resource.suggestions, false);
         _this.fillTemplate(_this.suggestion.detailsContainer, componentsTemplate[_this.suggestionDetailTemplatePath], resource.suggestions, false);
     
         $(document).trigger('suggestion-loaded');
     }
     
-    Suggestions.prototype.getSuggestions = function() {
-        var _this = this;
-            _this.suggestionListTemplatePath = 'suggestion-list';
-            _this.suggestionDetailTemplatePath = 'suggestion-detail';
-        _this.clmQueryRecord(_this.queryConfig.suggestions, _this.parseSuggestions(suggestions));
-    }
-
     Suggestions.prototype.buildSuggestions = function() {
         var _this = this;
-            _this.suggestionListTemplatePath = 'suggestion-list';
-            _this.suggestionDetailTemplatePath = 'suggestion-detail';
             _this.suggestionDataPath = '/staticJson/suggestions.json';
-        /*_this.fetchResource(_this.suggestionDataPath, 'json').then(function() {
-            _this.renderSuggestions();
-        });*/
-        
+      
         $.ajax({
             method: 'GET',
             url: _this.suggestionDataPath,
             type: 'json',
             success: function(data) {
-                var path = this.url;
                 resource.suggestions = data;
                 _this.renderSuggestions();
             },
@@ -31330,7 +31315,14 @@ $(function() {
         if (_this.application != 'iRep') {
             _this.buildSuggestions();
             try {
-                _this.clmQueryRecord(_this.queryConfig.suggestions, _this.parseSuggestions(suggestions));
+                _this.clmQueryRecord(_this.queryConfig.suggestions, function(result) {
+                    if (result.success == true) {
+                        _this.consoleLog('suggestion', result[_this.queryConfig.suggestions.object]);
+                        _this.parseSuggestions(result[_this.queryConfig.suggestions.object]);
+                    } else {
+                        _this.consoleLog('suggestion - response', result);
+                    }
+                });
             } catch (error) {
                 _this.consoleLog('Error', error);
             }
--- AZ_Suggestions_Rep_Territory/js/suggestion.js
+++ AZ_Suggestions_Rep_Territory/js/suggestion.js
@@ -19,36 +19,21 @@
      Suggestions.prototype.renderSuggestions = function() {
         var _this = this;
         //fillTemplate(container, templateObj, object, appendFlag, callback)
-    //    _this.fillTemplate(_this.suggestion.listContainer, componentsTemplate[_this.suggestionListTemplatePath], resource[_this.suggestionDataPath], false);
-    //    _this.fillTemplate(_this.suggestion.detailsContainer, componentsTemplate[_this.suggestionDetailTemplatePath], resource[_this.suggestionDataPath], false);
         _this.fillTemplate(_this.suggestion.listContainer, componentsTemplate[_this.suggestionListTemplatePath], resource.suggestions, false);
         _this.fillTemplate(_this.suggestion.detailsContainer, componentsTemplate[_this.suggestionDetailTemplatePath], resource.suggestions, false);
     
         $(document).trigger('suggestion-loaded');
     }
     
-    Suggestions.prototype.getSuggestions = function() {
-        var _this = this;
-            _this.suggestionListTemplatePath = 'suggestion-list';
-            _this.suggestionDetailTemplatePath = 'suggestion-detail';
-        _this.clmQueryRecord(_this.queryConfig.suggestions, _this.parseSuggestions(suggestions));
-    }
-
     Suggestions.prototype.buildSuggestions = function() {
         var _this = this;
-            _this.suggestionListTemplatePath = 'suggestion-list';
-            _this.suggestionDetailTemplatePath = 'suggestion-detail';
             _this.suggestionDataPath = '/staticJson/suggestions.json';
-        /*_this.fetchResource(_this.suggestionDataPath, 'json').then(function() {
-            _this.renderSuggestions();
-        });*/
-        
+      
         $.ajax({
             method: 'GET',
             url: _this.suggestionDataPath,
             type: 'json',
             success: function(data) {
-                var path = this.url;
                 resource.suggestions = data;
                 _this.renderSuggestions();
             },
@@ -70,7 +55,14 @@
         if (_this.application != 'iRep') {
             _this.buildSuggestions();
             try {
-                _this.clmQueryRecord(_this.queryConfig.suggestions, _this.parseSuggestions(suggestions));
+                _this.clmQueryRecord(_this.queryConfig.suggestions, function(result) {
+                    if (result.success == true) {
+                        _this.consoleLog('suggestion', result[_this.queryConfig.suggestions.object]);
+                        _this.parseSuggestions(result[_this.queryConfig.suggestions.object]);
+                    } else {
+                        _this.consoleLog('suggestion - response', result);
+                    }
+                });
             } catch (error) {
                 _this.consoleLog('Error', error);
             }
